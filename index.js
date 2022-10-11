d20daea117278e2c5a63b6fbf470495bf9dfbeec
letoor1234 <angelmartinezwb@gmail.com>
2022-10-11 18:33:31 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// Esto es un comentario en linea
+/*
+    Esto es un comentario
+    en bloque
+*/
+// VARIABLES
+/*
+    Es un espacio en la
+    memoria de nuestro archivo
+    que almacena un valor
+*/
+// const, let (palabra reservada)
+// texto (nombre de nuestra variable)
+// "Esto es..." (valor de nuestra variable)
+const texto = "Esto es un texto"; // tipo string
+const numero = 1.5;               // tipo number
+const booleano = false;           // tipo boolean (false, true)
+
+// console.log(numero);
+
+// FUNCIONES
+/*
+    Bloques que almacenan la lógica 
+    de un proyecyo,
+    son reutilizables
+*/
+// function (palabra reservada)
+// myFuncion (nombre de nuestra variable)
+// nombre (parámetro, variable que utiliza la funcion de forma interna)
+// { } (bloque de codigo)
+// Declaración (o iniciacion)
+function myFuncion(nombre) {
+    console.log("Hola, ", nombre, ", que tal tu día?");
+}
+// Invocación 
+/*
+    Es la utilización de una función
+    que se escribió previamente
+*/
+myFuncion("David");
+myFuncion("Angel");
+myFuncion("Javito");
---
+++ suma.js
@@ -0,0 +1,26 @@
+// declaramos variables
+const numero1 = 10;
+const numero2 = 15;
+// creamos funcion
+function sumar(a, b) {
+    // return 
+    /*
+        es una forma de que nuestra funcion
+        retorne un valor que puede ser almacenado
+        y utilizado
+    */
+    return a + b;
+};
+
+// invocamos la funcion y almacenamos el retorno en una variable
+const resultado = sumar(numero1, numero2);
+
+// mostramos el resultado en consola
+console.log(resultado);
+
+// OPERADORES MATEMATICOS
+// + (suma)
+// - (resta)
+// * (multiplicacion)
+// / (division)
+// % (resto de una division)
